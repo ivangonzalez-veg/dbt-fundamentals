6a0d672eed0c85d943d11354547362b367459bed
ivangonzalez@veg.vet <ivangonzalez@veg.vet>
2022-01-16 11:50:26 +0000

    added freshness

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,12 @@
+
+with payments as (
+    select * from {{ref('stg_payments')}}
+),
+orders as(
+    select * from {{ref('stg_orders')}}
+),
+final as (
+    select orders.order_id, orders.customer_id, payments.amount
+    from orders inner join payments on orders.order_id = payments.orderid
+)
+select * from final
